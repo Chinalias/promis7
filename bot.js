d6005e40f029d89ca6daccc2516b450b5458c33b
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-08-24 14:29:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
  
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '1';
+const prefix = 'Type 7play';
 client.on('ready', function() {
     client.user.setGame(` !help - skoza Music`,"http://twitch.tv/Baktash_183")
     console.log(`i am ready ${client.user.username}`);
